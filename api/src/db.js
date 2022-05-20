6154554aeb6906d39fbb961453b0e1a6a55794b7
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-05-20 20:31:39 -0300

    creation of database

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Diet } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Recipe.belongsToMany(Diet, {through: 'recipe_diet'}) // tabla intermedia
+Diet.belongsToMany(Recipe, {through: 'recipe_diet'})
+
+//las recetas pueden tener varios tipos de dieta, y los dietas pueden tener varias recetas
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
