b8f46f95e10ddfd11eb177157264c28a5c2e9ac3
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-06-09 21:54:54 -0300

    modify the route for the user to add a non-existent diet

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -4,8 +4,7 @@ const { Recipe, Diet } = require("../db"); //importo los modelos
 
 router.post("/", async (req, res, next) => {
   try {
-    let { title, summary, healthScore, steps, diets} =
-      req.body;
+    let { title, summary, healthScore, steps, diets } = req.body;
 
     let recipeCreate = await Recipe.create({
       title,
@@ -14,21 +13,21 @@ router.post("/", async (req, res, next) => {
       steps,
     });
 
-
     for (let i = 0; i < diets.length; i++) {
-    //en lugar de utilizar findOne, usar findOrCreate 
-      let diet = await Diet.findOne({                
-        where: { name: diets[i] },
+      //devuelvo un array con la variable dietas (contiene los datos encontrados o recien creados) y la variable created (es un booleano que indica si es creado o no)
+      let [diet, created] = await Diet.findOrCreate({
+        where: {
+          name: diets[i],
+        },
       });
 
-      recipeCreate.addDiet(diet);
+     recipeCreate.addDiet(diet);
     }
 
     res.status(200).send("recipe created successfully");
   } catch (error) {
     next(error);
   }
-
 });
 
 module.exports = router;
