7ac23c8e620cb09752e970e3429c598519e6ef25
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-04 19:07:32 -0300

    fix bug

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getRecipeByID, deleteRecipe, reset } from "../actions";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import defaultImg from "../utils/image-not-found.png";
-import Loader from "../components/Loader";
+import Loader from "./Loader";
 import s from "../styles/Details.module.css";
 import logoVeggie from "../utils/veggie.png";
 import brocco from "../utils/piece-of-broccoli.png";
@@ -28,29 +28,32 @@ export default function Details() {
     navigate("/recipes");
   };
 
+  console.log(details.length)
+
   return (
-    <>
+    <div>
       <div className={s.container}>
         {typeof details.id === "string" && (
           <button onClick={handleDelete} className={s.delete}> <span className={s.iconDelete}>❌</span></button>
         )}
         {
-          details.length === 0 ? (
+          details.length <= 0 ? (
             <div className={s.spinnerContainer}>
-              <Loader />
+             <Loader/>
             </div>
           ) : (
             //  : details.length > 0 ? (
 
             <div className={s.container}>
-              <p className={s.title}>{details.title}</p>
+              <p className={s.title}>{details?.title}</p>
 
+              <div>
               <div className={s.imageContainer}>
-                {details.image ? (
-                  <img src={details.image} className={s.img} alt="not found" />
-                ) : (
+                {/* {details?.image ? ( */}
+                  <img src={details?.image} className={s.img} alt="not found" />
+                {/* ) : (
                   <img src={defaultImg} className={s.img} height='350px' alt="default" />
-                )}
+                )} */}
               </div>
 
               <div className={s.type}>
@@ -58,25 +61,27 @@ export default function Details() {
                 <h3 className={s.typeItems}>
                   {details.diets && details.diets.map((el) => el.name +  " | ")}
                   {/* <h4 className={s.vegetarian}> */}
-                    {details.vegetarian === true ? "Vegetarian" : " "}
+                    {details?.vegetarian === true ? "Vegetarian" : " "}
                   {/* </h4> */}
                 </h3>
+              </div>
               </div>
 
               <div className={s.upContainer}>
                 <div className={s.scoreContainer}>
                   <h5 className={s.score}>Score</h5>
-                  <h3>{details.healthScore}⭐</h3>
+                  <h3>{details?.healthScore}⭐</h3>
                 </div>
 
-                <div className={s.dish}>
+                {/* <div className={s.dish}>
                   <h5 className={s.dishTitle}>Dish Type</h5>
                   <h4 className={s.dishT}>
-                    {details.dishTypes
+                    {details?.dishTypes
                       ? details.dishTypes.map((d) => d.name + " ")
                       : "not found"}
                   </h4>
-                </div>
+                </div> */}
+
               </div>
 
               <img className={s.logoVeggie} src={logoVeggie} alt="veggie" />
@@ -86,7 +91,7 @@ export default function Details() {
                 <h4>
                   <div
                     className={s.summaryText}
-                    dangerouslySetInnerHTML={{ __html: details.summary }}
+                    dangerouslySetInnerHTML={{ __html: details?.summary }}
                   />
                 </h4>
               </div>
@@ -97,7 +102,7 @@ export default function Details() {
                 <h3 className={s.stepTitle}>Steps:</h3>
                 <ul>
                   <h4>
-                    {Array.isArray(details.steps)
+                    {Array.isArray(details?.steps)
                       ? details.steps.map((e) =>
                           e.steps.map((f) => (
                             <li className={s.stepItem} key={f.number}>
@@ -105,7 +110,7 @@ export default function Details() {
                             </li>
                           ))
                         )
-                      : details.steps}
+                      : details?.steps}
                   </h4>
                 </ul>
               </div>
@@ -121,6 +126,6 @@ export default function Details() {
           //   <p>Loading...</p>
         }
       </div>
-    </>
+    </div>
   );
 }
