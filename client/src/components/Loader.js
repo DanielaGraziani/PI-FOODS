b6da57ba130e6f216d40b77d402f0eb68c0aa012
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-04 19:06:35 -0300

    fix bug

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from '../styles/Loader.module.css'
 
-export default function () {
+export default function Loader() {
   return (
     <div className={s.spinnerContainer}>
         <div className={s.spinner}></div>
