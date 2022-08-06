93e93b27d27449141d2d5ed30fd00c71b90ccda8
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2022-08-06 02:17:40 -0300

    Add minutes and servings

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -18,9 +18,9 @@ export default function Details() {
 
   useEffect(() => {
     dispatch(getRecipeByID(id));
-    return()=>{
-      dispatch(reset())
-    }
+    return () => {
+      dispatch(reset());
+    };
   }, [dispatch, id]);
 
   const handleDelete = () => {
@@ -28,103 +28,80 @@ export default function Details() {
     navigate("/recipes");
   };
 
-  console.log(details.length)
+  console.log(details.length);
 
   return (
     <div>
       <div className={s.container}>
         {typeof details.id === "string" && (
-          <button onClick={handleDelete} className={s.delete}> <span className={s.iconDelete}>❌</span></button>
+          <button onClick={handleDelete}>
+            <span>❌</span>
+          </button>
         )}
-        {
-          details.length <= 0 ? (
-            <div className={s.spinnerContainer}>
-             <Loader/>
-            </div>
-          ) : (
-            //  : details.length > 0 ? (
+        {details && details.length <= 0 ? (
+          <div className={s.spinnerContainer}>
+            <Loader />
+          </div>
+        ) : (
+          <div className={s.title}>
+            <h2>{details?.title}</h2>
 
-            <div className={s.container}>
-              <p className={s.title}>{details?.title}</p>
-
-              <div>
+            <div>
               <div className={s.imageContainer}>
-                {/* {details?.image ? ( */}
-                  <img src={details?.image} className={s.img} alt="not found" />
-                {/* ) : (
-                  <img src={defaultImg} className={s.img} height='350px' alt="default" />
-                )} */}
+                <img src={details.image} className={s.img} alt="not found" />
               </div>
 
               <div className={s.type}>
-                {/* <h3 className={s.typeTitle}>Type of Diet</h3> */}
-                <h3 className={s.typeItems}>
-                  {details.diets && details.diets.map((el) => el.name +  " | ")}
-                  {/* <h4 className={s.vegetarian}> */}
-                    {details?.vegetarian === true ? "Vegetarian" : " "}
-                  {/* </h4> */}
-                </h3>
-              </div>
+                {details.diets &&
+                  details.diets.map((el) => (
+                    <p className={s.typeItems}>{el.name}</p>
+                  ))}
               </div>
 
-              <div className={s.upContainer}>
-                <div className={s.scoreContainer}>
-                  <h5 className={s.score}>Score</h5>
-                  <h3>{details?.healthScore}⭐</h3>
+              <div className={s.scoreContainer}>
+                <div className={s.healthScore}>⭐{details?.healthScore}</div>
+                <div className={s.clock}>
+                  ⏰{details.readyInMinutes} <span className={s.min}>Min</span>
                 </div>
-
-                {/* <div className={s.dish}>
-                  <h5 className={s.dishTitle}>Dish Type</h5>
-                  <h4 className={s.dishT}>
-                    {details?.dishTypes
-                      ? details.dishTypes.map((d) => d.name + " ")
-                      : "not found"}
-                  </h4>
-                </div> */}
-
+                <div className={s.person}>
+                  🍽️{details.servings}{" "}
+                  <span className={s.servings}>Servings</span>
+                </div>
               </div>
-
-              <img className={s.logoVeggie} src={logoVeggie} alt="veggie" />
-
-              <div className={s.summaryContainer}>
-                <h2 className={s.summaryTitle}>Summary</h2>
-                <h4>
-                  <div
-                    className={s.summaryText}
-                    dangerouslySetInnerHTML={{ __html: details?.summary }}
-                  />
-                </h4>
-              </div>
-
-              <img className={s.brocco} src={brocco} alt="broccoli" />
-
-              <div className={s.stepContainer}>
-                <h3 className={s.stepTitle}>Steps:</h3>
-                <ul>
-                  <h4>
-                    {Array.isArray(details?.steps)
-                      ? details.steps.map((e) =>
-                          e.steps.map((f) => (
-                            <li className={s.stepItem} key={f.number}>
-                              {f.number} - {f.step}
-                            </li>
-                          ))
-                        )
-                      : details?.steps}
-                  </h4>
-                </ul>
-              </div>
-
-              <img className={s.pencil} src={pencil} alt="spatula" />
-              <Link to={"/recipes"}>
-                <button className={s.button}> Go Back </button>
-              </Link>
             </div>
-          )
 
-          // ) :
-          //   <p>Loading...</p>
-        }
+            <div className={s.summaryContainer}>
+              <h3 className={s.summaryTitle}>Summary</h3>
+              <div
+                className={s.summaryText}
+                dangerouslySetInnerHTML={{ __html: details?.summary }}
+              />
+            </div>
+
+            <img className={s.brocco} src={brocco} alt="broccoli" />
+            <div className={s.stepContainer}>
+              <h3 className={s.stepTitle}>Steps:</h3>
+              <ul>
+                <h4>
+                  {Array.isArray(details?.steps)
+                    ? details.steps.map((e) =>
+                        e.steps.map((f) => (
+                          <li className={s.stepItem} key={f.number}>
+                            {f.number} - {f.step}
+                          </li>
+                        ))
+                      )
+                    : details?.steps}
+                </h4>
+              </ul>
+            </div>
+
+            <img className={s.pencil} src={pencil} alt="spatula" />
+            <Link to={"/recipes"}>
+              <button className={s.button}> Go Back </button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
